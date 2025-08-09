e055f11d069e92c9372f1425440d74e01e419cac
KhymNad <87257632+KhymNad@users.noreply.github.com>
2025-08-08 21:24:28 -0600

    deployment fixes

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { motion, useInView } from 'framer-motion';
 import styles from './SkillsSection.module.css';
 
